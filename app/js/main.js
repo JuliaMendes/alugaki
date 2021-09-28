e524f155f2cc4d5f0b543aa76ddea0f18ef522ec
JuliaMendes <juliadepaivams@gmail.com>
2021-09-28 20:32:15 -0300

    alteraçoes js

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,10 @@
+// fetch('https://rj.olx.com.br/?o=2&q=barraca', { mode: 'no-cors' })
+//     .then((resultado) => {
+//         return resultado.json();
+//     }).then((t) => {
+//         console.log(t);
+//     })
+
 document.addEventListener("click", e => {
     const isDropdownButton = e.target.matches("[data-dropdown-button]")
     if (!isDropdownButton && e.target.closest("[data-dropdown]") != null) return
