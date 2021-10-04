0012284004813cb95ba5166697d457684cf245f4
JuliaMendes <juliadepaivams@gmail.com>
2021-10-04 17:23:22 -0300

    alterando o consumo da api

--- app/js/main.js
+++ app/js/main.js
@@ -37,7 +37,7 @@ function busca() {
 }
 
 let menuBotoesCategoria = document.querySelectorAll('.botoes-categoria button')
-let valorBtn;
+let valorBtn = 'Esporte e Lazer';
 
 menuBotoesCategoria.forEach((botaoQueEuAcabeiDeClicar) => {
     botaoQueEuAcabeiDeClicar.addEventListener("click", () => {
@@ -57,45 +57,46 @@ menuBotoesCategoria.forEach((botaoQueEuAcabeiDeClicar) => {
 
 function chamaProdutos() {
     fetch('https://raw.githubusercontent.com/JuliaMendes/alugaki/main/app/database/db.json')
-    .then(response => response.json())
-    .then(data => {
-        // console.log(data)
-        const listaProdutos = document.querySelector('.lista-produtos')
+        .then(response => response.json())
+        .then(data => {
+            const listaProdutos = document.querySelector('.lista-produtos')
 
-    
-        function categoria(elemento) {
-            console.log(elemento.category, valorBtn)
-            return elemento.category  === valorBtn
-        }
-    
-    
-        data.products
-            .filter(categoria)
-            .forEach(elemento => {
-    
-        listaProdutos.innerHTML += `<div class="card-produto">
-        <div class="thumb">
-            <a href="listagem-prod.html"><img src="${elemento.img}" alt="${elemento.titulo}"></a>
-        </div>
-        <div class="info">
-            <h3>${elemento.titulo}</h3>
-            <div>
-                <img src="img/Star 1.png" alt="Ícone estrela">
-                <div>
-                    <small class="pontuacao">${elemento.avaliacao}</small>
-                    <small>• ${elemento.navaliacao} avaliações</small>
-                </div>
-            </div>
-            <div class="localiz">
-                <img src="img/location.png" alt="ícone localização">
-                <small>${elemento.localizacao}</small>
-            </div>
-            <h3 class="preco">${elemento.preco}</h3>
-        </div>
-        </div>`;
-        })
-    });
+
+            function categoria(elemento) {
+                console.log(elemento.category, valorBtn)
+                return elemento.category === valorBtn
+            }
+
+
+            listaProdutos.innerHTML = '';
+            data.products
+                .filter(categoria)
+                .slice(0, 4)
+                .forEach(elemento => {
+
+                    listaProdutos.innerHTML += `<div class="card-produto">
+                        <div class="thumb">
+                            <a href="listagem-prod.html"><img src="${elemento.img}" alt="${elemento.titulo}"></a>
+                        </div>
+                        <div class="info">
+                            <h3>${elemento.titulo}</h3>
+                            <div>
+                                <img src="img/Star 1.png" alt="Ícone estrela">
+                                <div>
+                                    <small class="pontuacao">${elemento.avaliacao}</small>
+                                    <small>• ${elemento.navaliacao} avaliações</small>
+                                </div>
+                            </div>
+                            <div class="localiz">
+                                <img src="img/location.png" alt="ícone localização">
+                                <small>${elemento.localizacao}</small>
+                            </div>
+                            <h3 class="preco">${elemento.preco}</h3>
+                        </div>
+                        </div>`;
+                })
+        });
 }
 
 
-
+chamaProdutos()
